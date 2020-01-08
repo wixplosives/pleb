dc065f49d3d360deee0e3273353cdda6a9ff09e5
Avi Vahl <avi.vahl@wix.com>
2020-01-08 16:40:29 +0200

    feat: update dep versions on snapshot build

--- src/publish.ts
+++ src/publish.ts
@@ -1,15 +1,16 @@
+import fs from 'fs';
 import childProcess from 'child_process';
 import { green, yellow, red } from 'chalk';
 import pacote from 'pacote';
-import { resolvePackages } from './resolve-packages';
+import { resolvePackages, INpmPackage } from './resolve-packages';
 
 const registry = `https://registry.npmjs.org/`;
 const cmdPublishText = `npm publish --registry ${registry}`;
 const cmdPublishTextNext = `npm publish --tag next --registry ${registry}`;
 
-const log = (message: string) => console.log(green('# ') + message);
-const logWarn = (message: string) => console.log(yellow('# ') + message);
-const logError = (message: string) => console.log(red('# ') + message);
+const log = (message: string) => console.log(`${green('#')} ${message}`);
+const logWarn = (message: string) => console.log(`${yellow('#')} ${message}`);
+const logError = (message: string) => console.log(red(`# ${message}`));
 
 export async function publish(contextPath: string): Promise<void> {
     const packages = await resolvePackages(contextPath);
@@ -34,36 +35,61 @@ async function publishIfRequired(packageName: string, packageVersion: string, pa
     try {
         const versions = await fetchPackageVersions(packageName);
         if (!versions.includes(packageVersion)) {
+            log(`${packageName}: ${cmdPublishText}`);
             childProcess.execSync(cmdPublishText, { cwd: packagePath, stdio: 'inherit' });
         } else {
             logWarn(`${packageName}: ${packageVersion} is already published. skipping.`);
         }
     } catch (error) {
-        logError(`${packageName}: error while publishing: ${error?.message || error}.`);
+        logError(`${packageName}: error while publishing: ${error?.stack || error}.`);
     }
 }
 
 export async function publishSnapshot(contextPath: string, commitHash: string): Promise<void> {
-    commitHash = commitHash.slice(0, 7);
-    const packages = await resolvePackages(contextPath);
-    for (const { directoryPath, packageJson, packageJsonPath } of packages) {
-        const { name: packageName, version: packageVersion } = packageJson;
+    const packages = resolvePackages(contextPath);
+    const validPackages: INpmPackage[] = [];
+    const packageToVersion = new Map<string, string>();
+    for (const npmPackage of packages) {
+        const { name: packageName, version: packageVersion, private: isPrivate } = npmPackage.packageJson;
         if (packageName === undefined) {
-            logWarn(`${packageJsonPath}: no "name" field. skipping.`);
+            logWarn(`${npmPackage.packageJsonPath}: no "name" field. skipping.`);
             continue;
-        } else if (packageJson.private) {
+        } else if (isPrivate) {
             logWarn(`${packageName}: private. skipping.`);
             continue;
         } else if (packageVersion === undefined) {
             logWarn(`${packageName}: no "version" field. skipping.`);
             continue;
         }
-        const cmdVersionText = `npm version ${packageVersion}-${commitHash}`;
-        log(`${packageName}: ${cmdVersionText}`);
-        childProcess.execSync(cmdVersionText, { cwd: directoryPath, stdio: 'inherit' });
+        validPackages.push(npmPackage);
+        packageToVersion.set(packageName, `${packageVersion}-${commitHash.slice(0, 7)}`);
+    }
+
+    for (const { packageJson, packageJsonPath, directoryPath } of validPackages) {
+        const { name: packageName, dependencies, devDependencies } = packageJson;
+        packageJson.version = packageToVersion.get(packageName!);
+        if (dependencies) {
+            overrideVersions(dependencies, packageToVersion);
+        }
+
+        if (devDependencies) {
+            overrideVersions(devDependencies, packageToVersion);
+        }
+
+        log(`${packageName}: updating versions in package.json`);
+        fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
         log(`${packageName}: ${cmdPublishTextNext}`);
         childProcess.execSync(cmdPublishTextNext, { cwd: directoryPath, stdio: 'inherit' });
         log(`${packageName}: done`);
+    }
+}
+
+function overrideVersions(dependencies: Record<string, string>, packageToVersion: Map<string, string>) {
+    for (const depName of Object.keys(dependencies)) {
+        const snapshotVersion = packageToVersion.get(depName);
+        if (snapshotVersion !== undefined) {
+            dependencies[depName] = snapshotVersion;
+        }
     }
 }
 
