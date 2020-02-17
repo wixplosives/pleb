64bce3def21fb78a2f4c4d2d960f2dcd791c037a
Avi Vahl <avi.vahl@wix.com>
2020-02-17 02:17:25 +0200

    test(fixture): only delete dist if exists should fix failure in CI

--- test/fixtures/dist-dir/build.js
+++ test/fixtures/dist-dir/build.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 const distPath = path.join(__dirname, 'npm');
 
-fs.rmdirSync(distPath, { recursive: true });
+if (fs.existsSync(distPath)) {
+    fs.rmdirSync(distPath, { recursive: true });
+}
 fs.mkdirSync(distPath, { recursive: true });
 fs.copyFileSync(require.resolve('./package.json'), path.join(distPath, 'package.json'));
 fs.writeFileSync(path.join(distPath, 'main.js'), `export default 'hello world';`);
