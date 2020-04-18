095cf8db93bb9ae3c55be386b3a0200403381d26
Avi Vahl <avi.vahl@wix.com>
2020-04-18 04:27:19 +0300

    test: simplify spawn test helper now uses events.once for readability

--- test/spawn-async.ts
+++ test/spawn-async.ts
@@ -1,25 +1,26 @@
 import { spawn, SpawnOptions } from 'child_process';
+import { once } from 'events';
 
 export interface ISpawnAsyncOptions extends SpawnOptions {
     pipeStreams?: boolean;
 }
 
 export async function spawnAsync(command: string, args: ReadonlyArray<string> = [], options: ISpawnAsyncOptions = {}) {
-    return new Promise<{ exitCode: number; output: string }>((res, rej) => {
-        const childProcess = spawn(command, args, options);
-        const output: Array<string | Buffer> = [];
-        childProcess.once('error', rej);
+    const childProcess = spawn(command, args, options);
+    const output: Array<string | Buffer> = [];
 
-        const captureOutput = output.push.bind(output);
-        if (childProcess.stdout && childProcess.stderr) {
-            childProcess.stdout.on('data', captureOutput);
-            childProcess.stderr.on('data', captureOutput);
+    const captureOutput = output.push.bind(output);
 
-            if (options.pipeStreams) {
-                childProcess.stdout.pipe(process.stdout);
-                childProcess.stderr.pipe(process.stderr);
-            }
+    if (childProcess.stdout && childProcess.stderr) {
+        childProcess.stdout.on('data', captureOutput);
+        childProcess.stderr.on('data', captureOutput);
+
+        if (options.pipeStreams) {
+            childProcess.stdout.pipe(process.stdout);
+            childProcess.stderr.pipe(process.stderr);
         }
-        childProcess.once('exit', (exitCode) => res({ output: output.join(), exitCode: exitCode || 0 }));
-    });
+    }
+
+    const [exitCode] = (await once(childProcess, 'exit')) as [number | null];
+    return { output: output.join(''), exitCode: exitCode || 0 };
 }
