74f3823ef4c2f9c04eb206c330f57b56d929b706
Avi Vahl <avi.vahl@wix.com>
2022-06-20 19:11:08 +0300

    feat(upgrade): allow skipping packages Introduce an optional `pleb.config.js/mjs/cjs` file.
    The only field currently available is `pinnedPackages`, documented in the README.
    
    Removed travis config from README. not using it and can't be sure it's still working.
    
    Indented upgrade report a bit to make it more readable.

---
+++ src/utils/config.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import { findFileUpSync, isPlainObject, isString } from '@wixc3/resolve-directory-context';
+
+export type SkipConfiguration = string | { name: string; reason?: string };
+
+export interface Configuration {
+  /**
+   * A list of packages to avoid upgrading when executing `pleb upgrade`.
+   * @example ['react', {name: 'execa', reason: 'native esm'}]
+   */
+  pinnedPackages?: SkipConfiguration[];
+}
+
+const possibleConfigNames = ['pleb.config.js', 'pleb.config.mjs', 'pleb.config.cjs'];
+
+export async function loadPlebConfig(directoryPath: string): Promise<Configuration> {
+  const host = { ...fs, ...path };
+  for (const configName of possibleConfigNames) {
+    const configFilePath = findFileUpSync(directoryPath, configName, host);
+    if (configFilePath !== undefined) {
+      const { default: configValue } = (await import(configFilePath)) as { default: unknown };
+      if (!isPlainObject(configValue)) {
+        throw new Error(`config file ${configFilePath} doesn't export a default object`);
+      }
+      return configValue as Configuration;
+    }
+  }
+  return {};
+}
+
+/** key is package to skip; value is reason (which can be an empty string) */
+export function normalizePinnedPackages(pinnedPackages: Configuration['pinnedPackages'] = []): Map<string, string> {
+  return new Map(
+    pinnedPackages.map((value) => {
+      return isString(value) ? [value, ''] : [value.name, value.reason ?? ''];
+    })
+  );
+}
