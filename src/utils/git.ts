04d446554ebad58c57edb1e04a89310874e4e2a9
Avi Vahl <avi.vahl@wix.com>
2025-07-03 15:31:13 +0300

    refactor: remove unused currentGitCommitHash

--- src/utils/git.ts
+++
@@ -1,6 +0,0 @@
-import { spawnSync } from 'node:child_process';
-
-export function currentGitCommitHash(cwd = process.cwd()): string | undefined {
-  const { stdout, status } = spawnSync('git', ['rev-parse', 'HEAD'], { encoding: 'utf8', cwd });
-  return status === 0 ? stdout.trim() : undefined;
-}
